de869ebc71171d176af36197a64b1a8c4a91020b
Claire Filipek <cfilipek@sva.edu>
2020-01-12 21:16:05 -0500

    Created components and routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Route, Redirect} from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+const App = () => (
+  <Router>
+    <Header/>
+    <Route  exact path='/' component={Home}></Route>
+    <Route  path='/about' component={About}></Route>
+    <Footer/>
+  </Router>
+)
 
 export default App;
+
