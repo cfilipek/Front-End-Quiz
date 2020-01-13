9e7c6a8edb5bcb97d3dc389ce75396dceea12293
Claire Filipek <cfilipek@sva.edu>
2020-01-13 18:47:30 -0500

    Added and styled SplashPage component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,32 @@
 import React, { Component }  from 'react';
+import SplashPage from './SplashPage';
 
 class Home extends Component {
+
+  state = {
+    display: "block",
+    displayHome: "none"
+  }
+
+  componentDidMount() {
+    this.id = setTimeout(() => this.setState({ display: "none", displayHome: "block" }), 5000)
+  }
+
+  componentWillUnmount() {
+    clearTimeout(this.id)
+  }
+
 
   render()
   {
     return(
     <div>
+      <div style={{display:`${this.state.display}`}}>
+        <SplashPage/>
+      </div>
+      <div style={{display:`${this.state.displayHome}`}}>
+        <div>Rest of homepage</div>
+      </div>
 
     </div>)}
 }
---
+++ src/components/SplashPage.js
@@ -0,0 +1,19 @@
+import React, { Component }  from 'react';
+
+class SplashPage extends Component {
+
+  render()
+  {
+    return(
+    <div className="splash-container">
+      <div className="splash-title animated flip delay-2s">
+        <div className="splash border animated fadeInDown delay-1s">
+          <h1>Welcome to Front-End Flash Cards</h1>
+          <h1>Let's ace the coding interview</h1>
+        </div>
+      </div>
+    </div>)}
+}
+
+
+export default SplashPage;
