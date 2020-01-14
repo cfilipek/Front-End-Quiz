a8d389d297d23374e7e06d5d0cdd85fd092793ab
Claire Filipek <cfilipek@sva.edu>
2020-01-14 16:08:48 -0500

    Updates

---
+++ precache-manifest.fe2d138585737186b2e64ef87b9c115d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32fcacda51071c587523f3be7aaba977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57a0156b2a87b0fe22a9",
+    "url": "/static/css/main.4e3792c0.chunk.css"
+  },
+  {
+    "revision": "3c0b4a21914847f05d39",
+    "url": "/static/js/2.df34d9d2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.df34d9d2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57a0156b2a87b0fe22a9",
+    "url": "/static/js/main.8aaf674f.chunk.js"
+  },
+  {
+    "revision": "10cd9a7f1014a84be8bb",
+    "url": "/static/js/runtime-main.eaf404d5.js"
+  }
+]);
